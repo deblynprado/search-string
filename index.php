3021feada4b2de374ad28d8f9ca8d1a91648be83
Deblyn Prado <deblynprado@hotmail.com>
2020-07-02 20:39:25 -0300

    Adding index page

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en-GB">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Search String | Search any string in any website</title>
+  <meta name="description" content="Search any string in any website" />
+  <meta name="robots" content="noindex, nofollow" />
+  <meta name="googlebot" content="noindex, nofollow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+  <meta name="bingbot" content="noindex, nofollow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+  <link rel="canonical" href="" />
+  
+  
+  
+  <body class="">
+    <!--[if IE]>
+      <div class="alert alert-warning">
+        You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" data-wpel-link="external" target="_blank" rel="nofollow external noopener noreferrer">upgrade your browser</a> to improve your experience.      </div>
+        <![endif]-->
+        
+        
+        <header class="header" role="header">
+          <div class="container">
+            <h1 class="title">Search String Tool</h1>
+            <p class="desc">This tool helps you to find a specific string in any website</p>
+          </div>
+        </header>
+        
+        <main class="main" role="main">
+          <div class="container">
+            <form action="#">
+              <div class="step-1">
+                <label for="input-string">Search for...</label>
+                <input type="text" name="input-string" id="input-string" class="input-string" placeholder="String that you want to find" />
+              </div>
+              
+              <div class="step-2">
+                <label for="websites-textarea">List of websites</label>
+                <textarea name="websites-textarea" id="websites-textarea" cols="50" rows="30"></textarea>
+              </div>
+              
+              <div class="submit-area">
+                <input type="submit" value="Submit">
+              </div>
+            </form>
+          </div>
+        </main>
+        
+        <footer class="footer" role="footer">
+          <div class="container">
+          </div>
+        </footer>
+      </body>
+      </html>
